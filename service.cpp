7ce8fd870e6de2a47e117719f99bbd1d7ad8a580
Matias Hermanrud Fjeld <mhf@hex.no>
2016-06-28 13:46:48 +0200

    service.cpp: Implemented simple TCP echo service

--- service.cpp
+++ service.cpp
@@ -1,6 +1,6 @@
 // This file is a part of the IncludeOS unikernel - www.includeos.org
 //
-// Copyright 2015 Oslo and Akershus University College of Applied Sciences
+// Copyright 2015-2016 Oslo and Akershus University College of Applied Sciences
 // and Alfred Bratterud
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,12 +15,62 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/*
+  An example to show incoming and outgoing TCP Connections.
+  In this example, IncludeOS is listening on port 80.
+
+  Data received on port 80 will be redirected to a
+  outgoing connection to a (in this case) python server (server.py)
+
+  Data received from the python server connection
+  will be redirected back to the client.
+
+  To try it out, use netcat to connect to this IncludeOS instance.
+*/
+
 #include <os>
-#include <cstdio>
+#include <net/inet4>
 
-void Service::start()
-{
+using Connection_ptr = net::TCP::Connection_ptr;
+using Disconnect = net::TCP::Connection::Disconnect;
 
-  printf("Hello world - OS included!\n");
+// An IP-stack object
+std::unique_ptr<net::Inet4<VirtioNet> > inet;
 
+void Service::start() {
+  // Assign a driver (VirtioNet) to a network interface (eth0)
+  hw::Nic<VirtioNet>& eth0 = hw::Dev::eth<0,VirtioNet>();
+
+  // Bring up a network stack, attached to the nic
+  inet = std::make_unique<net::Inet4<VirtioNet> >(eth0);
+
+  // FIXME: Remove static config?
+  // Static IP configuration, until we (possibly) get DHCP
+  inet->network_config( { 10,0,0,42 },      // IP
+                        { 255,255,255,0 },  // Netmask
+                        { 10,0,0,1 },       // Gateway
+                        { 8,8,8,8 } );      // DNS
+
+  // Set up a TCP server on port 80
+  auto& server = inet->tcp().bind(80);
+  inet->dhclient()->on_config([&server](bool timeout) {
+    if(!timeout)
+      printf("Server IP updated: %s\n", server.local().to_string().c_str());
+  });
+  printf("Server listening: %s \n", server.local().to_string().c_str());
+  // When someone connects to our server
+  server
+    .onDisconnect([](net::TCP::Connection_ptr client, Disconnect reason) {
+        printf("Disconnected: %s: %s\n",
+               client->to_string().c_str(), reason.to_string().c_str());
+        client->close();
+      })
+    .onConnect([](net::TCP::Connection_ptr client) {
+      printf("Connected: %s\n", client->to_string().c_str());
+      client->read(1024, [client](auto buf, size_t size) {
+          std::string data{(char *) buf.get(), size};
+          printf("Received: %s%s", data.c_str(), (data.back() == '\n') ? "" : "\n");
+          client->write(data.data(), data.size());
+        });
+      });
 }
