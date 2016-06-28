6bbf5a951bbf4c89c77759587585bfcc76e50e91
Matias Hermanrud Fjeld <mhf@hex.no>
2016-06-28 14:53:18 +0200

    Implemented TCP echo service in node.js

---
+++ service.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+var server = net.createServer()
+  .on('listening', () => {
+      console.log("Listening on %s", server.address().port);
+    })
+  .on('connection', (client) => {
+    var client_addr = client.remoteAddress + ':' + client.remotePort;
+    console.log("Connect: %s", client_addr);
+
+    client
+      .on('data', (buffer) => {
+          data = buffer.toString();
+          console.log("[%s] %s", client_addr,
+                      data.endsWith('\n') ? data.slice(0, -1) : data);
+          client.write(data);
+        })
+      .on('end', () => {
+          console.log("Disconnect: %s", client_addr);
+        })
+      .on('error', (err) => {
+          console.log('Error: %s: %s', client_addr, err.message);
+        })
+      })
+  .listen(8080);
